e6d425d37d740bc99c3cd6b646a53d9f88968336
Vishwajeet Dubey <vishwajeet.dubey.s63@kalvium.community>
2025-04-17 10:13:30 +0530

    changes

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,1 @@
-const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  location: String,
-  workoutType: String,
-  experienceLevel: String,
-  availability: [String], // e.g., ['Morning', 'Evening']
-});
-
-module.exports = mongoose.model('User', userSchema);
